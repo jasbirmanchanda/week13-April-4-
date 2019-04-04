8402fd68e5321c424e895bacf269744e73629487
jasbirmanchanda <742733@student.tor.loc>
2019-04-04 11:05:57 -0400

    print sum

--- printnumbers.js
+++ printnumbers.js
@@ -1,6 +1,14 @@
 let i = 0;
-while (i <= 11)
+/*while (i <= 11)        for printing numbers 1 to 11
 {
     console.log(i)
     i++;
+}*/
+
+let sum = 0;
+while (i <= 11)
+{
+    sum = sum + i;
+    i++;
 }
+console.log(sum);
