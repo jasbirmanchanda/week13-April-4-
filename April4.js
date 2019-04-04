678a565075962da90302b2c4a61fdf83d6dace6c
jasbirmanchanda <742733@student.tor.loc>
2019-04-04 10:50:20 -0400

    cas

--- April4.js
+++ April4.js
@@ -1,3 +1,4 @@
 let a= 9;
 let b = a * 2;
+let c = b +3;
 console.log(b);
---
+++ new.js
@@ -0,0 +1,10 @@
+var user = {
+    first_name: "CSS",
+    last_name: "HTML",
+    age: 4,/*from W W  W.java @ s  .c  o m*/
+    website: "java2s.com"
+};
+
+for (key in user) {
+    console.log(key);
+}
